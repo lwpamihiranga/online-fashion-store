00b8fa6304ff0e598820413c82e0472afb8e3671
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-07 20:19:28 +0530

    product route recreated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const productRouter = require('./routes/products'); 
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
+mongoose.connect('mongodb://localhost:27017/fashion_store', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/products', productRouter);
+
+app.use((req, res, next) => {
+    const error = new Error();
+    error.message = 'Not Found'
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(500).    json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 app.listen(3000);
