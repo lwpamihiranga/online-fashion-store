ef5c571b40b3872e39b6a7dd9ba058b020cbaf83
SBA <buddikasamesh1@gmail.com>
2020-05-01 22:54:29 +0530

    UI changes

--- frontend/src/components/main/productDetailsView.js
+++ frontend/src/components/main/productDetailsView.js
@@ -28,11 +28,15 @@ class App extends React.Component
            <div className="container">
                <div className="productDetails">
                    <img id="productImage" src="https://scontent.fcmb1-1.fna.fbcdn.net/v/t1.0-9/p720x720/93794211_215217119927900_2555796898816458752_o.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=xAkyRz7bRTsAX-a6X_w&_nc_ht=scontent.fcmb1-1.fna&_nc_tp=6&oh=48baa480cc116ab35d45fb24c3a153be&oe=5ECBD8C8"/>
-                   <div>
-                       <h1>{name}</h1>
-                       <h1>{price}</h1>
+                   <div className="productInfoContainer">
+                       <h3 id="productName">{name}</h3>
+                       <h3 id="productPrice">{"Rs:"+price + "/="}</h3>
                    </div>
 
+               </div>
+               <div className="buttonContainer">
+                   <input id="buyButton" type="button" value="Buy Now"/>
+                   <input id="addToWishListButton" type="button" value="Add to WishList"/>
                </div>
                <div className="ratings">
                    <RatingList productId = {productId}/>
