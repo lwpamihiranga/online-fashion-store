32fa94c018025425df95a2d1e357acdb7d5de43e
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-07 15:40:20 +0530

    old wishList model deleted

--- backend/src/models/wishlist.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const wishListSchema = mongoose.Schema({
-    productId : String,
-    userId : String
-});
-
-module.exports = mongoose.model('WishList', wishListSchema);
