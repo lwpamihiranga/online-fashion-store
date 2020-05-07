b1f64393e54bde1f284e5d8b5780b5bc3f7a5da8
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-07 19:45:03 +0530

    user model updated

--- backend/src/resources/user/user.model.js
+++ backend/src/resources/user/user.model.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name : String,
-    email : String,
-    password : String,
-    type : String,
-    imageLink : String
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: { type: String, required: true },
+    type: { type: String, enum: ['admin', 'manager', 'user'], default: 'user' },
+    imageLink: { type: String },
 });
 
 module.exports = mongoose.model('User', userSchema);
