0c3ec764d5ffd27c817c7cbd330729cef976afd6
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-07 15:40:09 +0530

    wishList resource files created

---
+++ backend/src/resources/wishList/wishList.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const wishListSchema = mongoose.Schema({
+    productId : String,
+    userId : String
+});
+
+module.exports = mongoose.model('WishList', wishListSchema);
