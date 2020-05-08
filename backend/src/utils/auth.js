f226dc24c274c6156ef27d14ee3ccddb42a79c69
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-09 01:01:59 +0530

    function name changed

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
  * @summary this function checks if the req is made by and admin, userTypes admin & manager
  * created for the purpose of only admins can add managers for the system
  */
-exports.checkAdmin = (req, res, next) => {
+exports.checkBeforeAddUser = (req, res, next) => {
     const userType = req.body.type || 'user';
 
     if (userType === 'admin' || userType === 'manager') {
