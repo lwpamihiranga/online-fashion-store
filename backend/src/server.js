d7e6fb14ae6b0af7d7ef0dfb99f45b5cee932511
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-08 12:56:27 +0530

    route not found msg updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,7 +39,7 @@ app.use('/api/cart', cartRouter);
 
 app.use((req, res, next) => {
     const error = new Error();
-    error.message = 'Not Found';
+    error.message = 'route not Found';
     next(error);
 });
 
