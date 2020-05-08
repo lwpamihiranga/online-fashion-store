ca672bc060410b647a96f9b440b75161dedc33b3
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-08 15:28:09 +0530

    mongoose import removed

--- backend/src/resources/user/user.router.js
+++ backend/src/resources/user/user.router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const userModel = require('./user.model');
 const UserController = require('./user.controller');
 
