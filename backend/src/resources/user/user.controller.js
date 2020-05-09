4090766414ca3f80b90fa784b503c59d4049df48
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-09 18:24:06 +0530

    controller updated

--- backend/src/resources/user/user.controller.js
+++ backend/src/resources/user/user.controller.js
@@ -28,8 +28,7 @@ exports.register = (req, res, next) => {
                             email: req.body.email,
                             password: hash,
                             type: req.body.type,
-                            imageLink:
-                                'https://scontent.fcmb1-1.fna.fbcdn.net/v/t1.0-9/p720x720/93794211_215217119927900_2555796898816458752_o.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=xAkyRz7bRTsAX-a6X_w&_nc_ht=scontent.fcmb1-1.fna&_nc_tp=6&oh=48baa480cc116ab35d45fb24c3a153be&oe=5ECBD8C8',
+                            imageLink: req.file.path,
                         });
 
                         user.save()
