aa0f5b373c334a3017889b3039580fc8d475185e
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-09 21:00:16 +0530

    model updated

--- backend/src/resources/product/product.model.js
+++ backend/src/resources/product/product.model.js
@@ -4,9 +4,12 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     categoryId: { type: String, required: true },
     description: { type: String, required: true },
+    hasDiscount: { type: Boolean, default: false },
     discount: { type: Number },
-    name: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
     price: { type: Number, required: true },
+    category: { type: String },
+    imageLink: { type: String, default: 'uploads/products/default.jpg' },
 });
 
 module.exports = mongoose.model('Product', productSchema);
