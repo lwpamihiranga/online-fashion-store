2b572630b503d84ce48b65ce7c608ffabfc9f858
samesh97 <buddikasamesh1@gmail.com>
2020-05-12 01:08:21 +0530

    UI changes

--- frontend/src/components/main/Login.js
+++ frontend/src/components/main/Login.js
@@ -18,8 +18,8 @@ class App extends React.Component
             <div className="loginContainer">
                 <div className="inputFieldContainer">
                     <img className="img-fluid mx-auto d-block loginImage" src={loginImage}/>
-                    <input class="form-control  mx-auto d-block mt-4 w-75 p-3" aria-label="Default" type="text" value={this.state.username}  onChange={(e)=> this.setState({username:e.target.value})} placeholder="Username"/>
-                    <input class="form-control  mx-auto d-block mt-2 w-75 p-3" aria-label="Default" type="password" value={this.state.password} onChange={(e)=> this.setState({password:e.target.value})} placeholder="Password"/>
+                    <input className="form-control  mx-auto d-block mt-4 w-75 p-4" aria-label="Default" type="text" value={this.state.username}  onChange={(e)=> this.setState({username:e.target.value})} placeholder="Username"/>
+                    <input className="form-control  mx-auto d-block mt-2 w-75 p-4" aria-label="Default" type="password" value={this.state.password} onChange={(e)=> this.setState({password:e.target.value})} placeholder="Password"/>
                     <input className="btn btn-primary mt-4 mx-auto d-block w-75" type="button" value="Login as user" onClick={() => this.checkLogin(this.state.username,this.state.password,"user")}/>
                     <input className="btn btn-primary mt-2 mx-auto d-block w-75"  type="button" value="Login as admin" onClick={() => this.checkLogin(this.state.username,this.state.password,"admin")}/>
                     <input className="btn btn-primary mt-2 mx-auto d-block w-75"  type="button" value="Login as store manager" onClick={() => this.checkLogin(this.state.username,this.state.password,"manager")}/>
