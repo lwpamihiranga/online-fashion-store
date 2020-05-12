7afe728b15b7589c5fde6bfd55d636478916e839
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-12 22:53:03 +0530

    formatting

--- frontend/src/components/main/Navigation.js
+++ frontend/src/components/main/Navigation.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import {BrowserRouter as Router,Link} from "react-router-dom";
-import Route from "react-router-dom/Route";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 //components
 import ProductDetailsView from './productDetailsView';
@@ -9,17 +9,14 @@ import Login from './Login';
 import Register from './Register';
 import WishList from './WishList';
 import Cart from './Cart';
-import Category from "./Category";
-import AdminCategoryView from "./AdminCategoryView";
-import ManagerView from "./StoreManagerProductView";
+import Category from './Category';
+import AdminCategoryView from './AdminCategoryView';
+import ManagerView from './StoreManagerProductView';
 
 import Roles from '../../_helpers/role';
 
-class App extends React.Component
-{
+class App extends React.Component {
     render() {
-
-
         //user login informations
         var name = localStorage.getItem('userName');
         var password = localStorage.getItem('userPassword');
@@ -30,97 +27,147 @@ class App extends React.Component
         //
 
         return (
-
-
             <Router>
-                <Route path="/"  render={
-                    () => {
+                <Route
+                    path="/"
+                    render={() => {
                         return (
                             <div className="navigationContainer">
                                 <h1 id="mainName">Fashion Store</h1>
-                                    <div className="navigation">
+                                <div className="navigation">
+                                    <Link
+                                        to={'/'}
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <p className="NavigationItem mt-3">
+                                            Home
+                                        </p>
+                                    </Link>
 
-
-                                        <Link to={"/"} style={{textDecoration: 'none'}}>
-                                            <p className="NavigationItem mt-3">Home</p>
+                                    {email != null && password != null && (
+                                        <p
+                                            className="NavigationItem mt-3"
+                                            onClick={this.LogoutUser}
+                                        >
+                                            Logout
+                                        </p>
+                                    )}
+                                    {email == null && password == null && (
+                                        <Link
+                                            to={'/login'}
+                                            style={{ textDecoration: 'none' }}
+                                        >
+                                            <p className="NavigationItem mt-3">
+                                                Login
+                                            </p>
                                         </Link>
-
-                                        {email != null && password != null &&
-                                            <p className="NavigationItem mt-3" onClick={this.LogoutUser}>Logout</p>
-                                        }
-                                        {email == null && password == null &&
-                                            <Link to={"/login"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">Login</p>
+                                    )}
+                                    {email == null && password == null && (
+                                        <Link
+                                            to={'/register'}
+                                            style={{ textDecoration: 'none' }}
+                                        >
+                                            <p className="NavigationItem mt-3">
+                                                Register
+                                            </p>
+                                        </Link>
+                                    )}
+                                    {type != null && type === Roles.User && (
+                                        <Link
+                                            to={'/cart'}
+                                            style={{ textDecoration: 'none' }}
+                                        >
+                                            <p className="NavigationItem mt-3">
+                                                Cart
+                                            </p>
+                                        </Link>
+                                    )}
+                                    {type != null && type === Roles.User && (
+                                        <Link
+                                            to={'/wishList'}
+                                            style={{ textDecoration: 'none' }}
+                                        >
+                                            <p className="NavigationItem mt-3">
+                                                WishList
+                                            </p>
+                                        </Link>
+                                    )}
+                                    {email != null &&
+                                        password != null &&
+                                        type === Roles.Admin && (
+                                            <Link
+                                                to={'/categories'}
+                                                style={{
+                                                    textDecoration: 'none',
+                                                }}
+                                            >
+                                                <p className="NavigationItem mt-3">
+                                                    Categories
+                                                </p>
                                             </Link>
-                                        }
-                                        {email == null && password == null &&
-                                            <Link to={"/register"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">Register</p>
+                                        )}
+                                    {email != null &&
+                                        password != null &&
+                                        type === Roles.StoreManager && (
+                                            <Link
+                                                to={'/products'}
+                                                style={{
+                                                    textDecoration: 'none',
+                                                }}
+                                            >
+                                                <p className="NavigationItem mt-3">
+                                                    Products
+                                                </p>
                                             </Link>
-                                        }
-                                        {type != null && type === Roles.User &&
-                                            <Link to={"/cart"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">Cart</p>
-                                            </Link>
-                                        }
-                                        {type != null && type === Roles.User &&
-                                            <Link to={"/wishList"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">WishList</p>
-                                            </Link>
-                                        }
-                                        {
-                                            email != null && password != null && type === Roles.Admin &&
-                                            <Link to={"/categories"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">Categories</p>
-                                            </Link>
-                                        }
-                                        {
-                                            email != null && password != null && type === Roles.StoreManager &&
-                                            <Link to={"/products"} style={{textDecoration: 'none'}}>
-                                                <p className="NavigationItem mt-3">Products</p>
-                                            </Link>
-                                        }
-
-
-                                    </div>
+                                        )}
+                                </div>
                             </div>
-
                         );
-                    }
-                }
+                    }}
                 />
-                <Route path={"/"} exact strict component={Category}/>
-                <Route path={"/product/:pid"} exact strict component={ProductDetailsView}/>
-                <Route path={"/login"} exact strict component={Login}/>
-                <Route path={"/register"} exact strict component={Register}/>
-                <Route path={"/wishList"} exact strict component={WishList}/>
-                <Route path={"/cart"} exact strict component={Cart}/>
-                <Route path={"/categories"} exact strict component={AdminCategoryView}/>
-                <Route path={"/products"} exact strict component={ManagerView}/>
-
+                <Route path={'/'} exact strict component={Category} />
+                <Route
+                    path={'/product/:pid'}
+                    exact
+                    strict
+                    component={ProductDetailsView}
+                />
+                <Route path={'/login'} exact strict component={Login} />
+                <Route path={'/register'} exact strict component={Register} />
+                <Route path={'/wishList'} exact strict component={WishList} />
+                <Route path={'/cart'} exact strict component={Cart} />
+                <Route
+                    path={'/categories'}
+                    exact
+                    strict
+                    component={AdminCategoryView}
+                />
+                <Route
+                    path={'/products'}
+                    exact
+                    strict
+                    component={ManagerView}
+                />
             </Router>
-        )
+        );
     }
-    LogoutUser()
-    {
-        localStorage.removeItem("userName");
-        localStorage.removeItem("userPassword");
-        localStorage.removeItem("userType");
-        localStorage.removeItem("userId");
-        localStorage.removeItem("userImageLink");
-        localStorage.removeItem("userEmail");
+    LogoutUser() {
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userPassword');
+        localStorage.removeItem('userType');
+        localStorage.removeItem('userId');
+        localStorage.removeItem('userImageLink');
+        localStorage.removeItem('userEmail');
 
-        window.location.href = "http://localhost:3000/";
+        window.location.href = 'http://localhost:3000/';
     }
-    getUser()
-    {
+    getUser() {
         var name = localStorage.getItem('userName');
         var password = localStorage.getItem('userPassword');
         var type = localStorage.getItem('userType');
         var id = localStorage.getItem('userId');
         var imageLink = localStorage.getItem('userImageLink');
         var email = localStorage.getItem('userEmail');
-
     }
 }
 export default App;
