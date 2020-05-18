437c50eafa29e2dd9ab22192d260d9346936bb03
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-19 01:03:36 +0530

    make changes to pass a prop and set the state

--- frontend/src/components/main/Category.js
+++ frontend/src/components/main/Category.js
@@ -2,87 +2,117 @@ import React from 'react';
 import axios from 'axios';
 import CategoryList from '../lists/categoryList';
 import GridView from '../lists/gridView';
-import style from '../../css/style.css'
+import style from '../../css/style.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             categoryList: [],
-            selectedCategory : '',
-            productList : []
+            selectedCategory: '',
+            productList: [],
         };
-        this.getCategoriesFromApi();
+        // this.getCategoriesFromApi();
+        // this.getProducts();
     }
 
     render() {
+        // const categories = this.state.categoryList.map(cat => {
 
-        const categories = this.state.categoryList.map(cat => {
+        //     if(this.state.selectedCategory === cat._id)
+        //     {
+        //         return(<option selected value={cat._id}>{cat.catName}</option>)
+        //     }
+        //     else
+        //     {
+        //         return(<option value={cat._id}>{cat.catName}</option>)
+        //     }
+        // });
 
-            if(this.state.selectedCategory === cat._id)
-            {
-                return(<option selected value={cat._id}>{cat.catName}</option>)
-            }
-            else
-            {
-                return(<option value={cat._id}>{cat.catName}</option>)
-            }
-        });
+        const categoryId = this.props.match.params.cid;
 
-
-        return(
+        if (categoryId !== undefined) {
+            console.log('ran', categoryId);
+            this.getProductsByCategory(categoryId);
+        } else {
+            this.getProducts();
+        }
+        return (
             <div className="productBody">
                 <div className="productDiv">
-                    <div>
-                        <select className="mt-2 mb-2"
-                                onChange={(e) => this.onCategoryChanged(e.target.value)}>
+                    {/* <div>
+                        <select className="mt-2 mb-2" onChange={(e) => this.onCategoryChanged(e.target.value)}>
                             {categories}
                         </select>
-                    </div>
-                    <GridView productList={this.state.productList} isCategory = {true}/>
+                    </div> */}
+                    <GridView productList={this.state.productList} isCategory={true} />
                 </div>
             </div>
-
-            )
+        );
     }
 
-    getCategoriesFromApi() {
-        //loading categories from the api to the state
+    // getCategoriesFromApi() {
+    //     //loading categories from the api to the state
+    //     axios
+    //         .get('http://localhost:5000/api/category/')
+    //         .then((response) => {
+    //             if (response.status === 200) {
+    //                 var list = response.data;
+
+    //                 list.map((itm,index) => {
+    //                     if(index === 0)
+    //                     {
+    //                         this.setState({ categoryList: list,selectedCategory : itm._id });
+    //                         this.onCategoryChanged( itm._id);
+    //                     }
+    //                 });
+
+    //             }
+    //         })
+    //         .catch((error) => console.log('Category fetch error: ', error));
+    // }
+    // onCategoryChanged = (catId) => {
+
+    //     this.setState({ selectedCategory: catId});
+
+    //     axios.get('http://localhost:5000/api/products/findByCategoryId?id=' + catId)
+    //         .then(res => {
+
+    //             if(res.status === 200)
+    //             {
+    //                 this.setState({productList : res.data});
+    //             }
+
+    //         })
+    //         .catch(error => {
+
+    //         });
+    // }
+
+    getProducts = () => {
         axios
-            .get('http://localhost:5000/api/category/')
-            .then((response) => {
-                if (response.status === 200) {
-                    var list = response.data;
+            .get('http://localhost:5000/api/products/')
+            .then((res) => {
+                console.log(res.data);
 
-                    list.map((itm,index) => {
-                        if(index === 0)
-                        {
-                            this.setState({ categoryList: list,selectedCategory : itm._id });
-                            this.onCategoryChanged( itm._id);
-                        }
-                    });
-
-
+                if (res.status === 200) {
+                    this.setState({ productList: res.data });
                 }
             })
-            .catch((error) => console.log('Category fetch error: ', error));
-    }
-    onCategoryChanged = (catId) => {
-
-        this.setState({ selectedCategory: catId});
-
-        axios.get('http://localhost:5000/api/products/findByCategoryId?id=' + catId)
-            .then(res => {
-
-                if(res.status === 200)
-                {
-                    this.setState({productList : res.data});
-                }
-
-            })
-            .catch(error => {
-
+            .catch((err) => {
+                console.log('Error: ', err);
             });
-    }
+    };
+
+    getProductsByCategory = (catId) => {
+        axios
+            .get('http://localhost:5000/api/products/findByCategoryId?id=' + catId)
+            .then((res) => {
+                if (res.status === 200) {
+                    this.setState({ productList: res.data });
+                }
+            })
+            .catch((error) => {});
+    };
 }
 export default App;
