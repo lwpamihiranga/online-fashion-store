70ceb9cc2b495e47ffce72a226dc5029d9533cf4
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-05-21 02:08:49 +0530

    new mail sender funt setup

--- backend/src/utils/mail-sender.js
+++ backend/src/utils/mail-sender.js
@@ -1,26 +1,53 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
-let transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-        user: 'df564336fc07e3',
-        pass: 'a895dc678bab7d',
-    },
-});
+// const nodemailer = require('nodemailer');
 
-exports.sendManagerMail = (senderMail, receiverMail, receiverName) => {
-    const message = {
-        from: senderMail,
+// let transport = nodemailer.createTransport({
+//     host: 'smtp.mailtrap.io',
+//     port: 2525,
+//     auth: {
+//         user: 'df564336fc07e3',
+//         pass: 'a895dc678bab7d',
+//     },
+// });
+
+// exports.sendManagerMail = (senderMail, receiverMail, receiverName) => {
+//     const message = {
+//         from: senderMail,
+//         to: receiverMail,
+//         subject: 'Manager Account Created',
+//         html: `<h3>Hello ${receiverName},</h3>
+//             <p>Your manager account has been created. Login to your account and start working.</p>
+//             <p>Thanks,<br/> Admin Team</p>`,
+//     };
+
+//     transport
+//         .sendMail(message)
+//         .then((info) => {
+//             console.log('Email sent! Info: ', info);
+//         })
+//         .catch((err) => {
+//             console.log('Cannot send the email, error: ', err);
+//         });
+// };
+
+exports.sendManagerMail = (receiverMail, receiverName) => {
+    sgMail.setApiKey(
+        process.env.SENDGRID_API_KEY ||
+            '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'
+    );
+
+    const msg = {
         to: receiverMail,
+        from: 'fashionstore@guerrillamail.net', // do not change this mail
         subject: 'Manager Account Created',
+        text: 'Your manager account has been created.',
         html: `<h3>Hello ${receiverName},</h3>
             <p>Your manager account has been created. Login to your account and start working.</p>
             <p>Thanks,<br/> Admin Team</p>`,
     };
-
-    transport
-        .sendMail(message)
+    sgMail
+        .send(msg)
         .then((info) => {
             console.log('Email sent! Info: ', info);
         })
